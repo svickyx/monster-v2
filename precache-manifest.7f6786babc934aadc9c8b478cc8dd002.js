4b093e426d173ec2aa96e739d6161f16a0a64f54
静赵 <vicky@jingzhaodeMacBook-Pro.local>
2020-08-27 00:43:25 +0800

    Updates

---
+++ precache-manifest.7f6786babc934aadc9c8b478cc8dd002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9fb9d759c0d342b686b1ba302e76c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d244fb7df215926a8a8",
+    "url": "/static/css/main.10f235bb.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d244fb7df215926a8a8",
+    "url": "/static/js/main.80451c75.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  }
+]);
